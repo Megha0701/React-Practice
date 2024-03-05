e42ae54187666867849b4f653843b145111e10fc
Megha Mathur <meghamathur2001@gmail.com>
2024-03-05 11:38:43 +0530

    final commit fir resuable card component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react'
-import ProfileCard from './ProfileCard';
+import React from "react";
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+import "bulma/css/bulma.css";
 
 function App() {
   return (
     <div>
       <div>Personal Digital Assistance</div>
-      <ProfileCard title="Alexa" handle="@alexa99"/>
-      <ProfileCard title="Cortana" handle="@cortana31"/>
-      <ProfileCard title="Siri" handle="@siri01"/>
+      {/* <img src={AlexaImage}/>
+      <img src={SiriImage}/>
+      <img src={CortanaImage}/> */}
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+            </div>
+            <div className="column is-4">
+              <ProfileCard title="Cortana" handle="@cortana31" image={CortanaImage} />
+            </div>
+            <div className="column is-4">
+              <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,23 +1,31 @@
-import React from 'react'
+import React from "react";
 
-function ProfileCard({ title , handle }) {
+function ProfileCard({ title, handle, image }) {
+  //WAY-3 Directly do as in arguments
+  //function ProfileCard({ title , handle })
 
-    //WAY-3 Directly do as in arguments
-    //function ProfileCard({ title , handle })
+  //WAY-2 destructuring thr props
+  //const { title , handle } = props;
 
-    //WAY-2 destructuring thr props
-    //const { title , handle } = props;
-
-    //WAY-1 Putting in variable first
-    // const title = props.title;
-    // const handle = props.handle;
+  //WAY-1 Putting in variable first
+  // const title = props.title;
+  // const handle = props.handle;
 
   return (
-    <div>
-      <div>Title is {title} </div>
-      <div>Handle is {handle}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="pda logo" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title id-4">{title} </p>
+          <p className="subtitle is-6">{handle}</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ProfileCard;
